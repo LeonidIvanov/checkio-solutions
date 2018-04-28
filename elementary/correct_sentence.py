6e41e7b494038387d5954755e2073180014a190a
LeonidIvanov <git@github.com:LeonidIvanov/python0709.git>
2018-04-28 20:06:25 +0300

    Deleted prints from "Correct Sentence" solution

--- elementary/correct_sentence.py
+++ elementary/correct_sentence.py
@@ -13,13 +13,9 @@ def correct_sentence(text: str) -> str:
 
 
 if __name__ == '__main__':
-    print("Example:")
-    print(correct_sentence("greetings, friends"))
-
     # These "asserts" are used for self-checking and not for an auto-testing
     assert correct_sentence("greetings, friends") == "Greetings, friends."
     assert correct_sentence("Greetings, friends") == "Greetings, friends."
     assert correct_sentence("Greetings, friends.") == "Greetings, friends."
     assert correct_sentence("hi") == "Hi."
     assert correct_sentence("welcome to New York") == "Welcome to New York."
-    print("Coding complete? Click 'Check' to earn cool rewards!")
