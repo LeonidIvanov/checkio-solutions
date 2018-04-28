d694f841c8590411153c17edc399a8f5d7e9b7d3
LeonidIvanov <git@github.com:LeonidIvanov/python0709.git>
2018-04-28 20:06:49 +0300

    Deleted prints from "Say Hi" solution

--- elementary/say_hi.py
+++ elementary/say_hi.py
@@ -9,4 +9,3 @@ if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
     assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", "First"
     assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", "Second"
-    print('Done. Time to Check.')
