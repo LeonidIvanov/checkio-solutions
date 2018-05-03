7a50f87a9fd9ee6179f2b059810a1bb69518619d
LeonidIvanov <git@github.com:LeonidIvanov/python0709.git>
2018-05-03 15:12:52 +0300

    Updated "Bigger Price" docstring

--- elementary/bigger-price.py
+++ elementary/bigger-price.py
@@ -1,7 +1,8 @@
 def bigger_price(limit, data):
     """You have a table with all available goods in the store. The data is represented as a list of dicts
 
-    Your mission here is to find the TOP most expensive goods. The amount we are looking for will be given as a first argument and the whole data as the second one
+    Your mission here is to find the TOP most expensive goods.
+    The amount we are looking for will be given as a first argument and the whole data as the second one
 
     Input: int and list of dicts. Each dicts has two keys "name" and "price"
 
