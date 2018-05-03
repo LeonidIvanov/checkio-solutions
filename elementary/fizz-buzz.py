20df312d293a778cf8295505ebfd9906619f1c7b
LeonidIvanov <git@github.com:LeonidIvanov/python0709.git>
2018-05-03 14:56:15 +0300

    Updated "Fizz Buzz" solution

--- elementary/fizz-buzz.py
+++ elementary/fizz-buzz.py
@@ -23,7 +23,7 @@ def fizz_buzz(number):
 
 
 if __name__ == '__main__':
-    assert checkio(15) == "Fizz Buzz", "15 is divisible by 3 and 5"
-    assert checkio(6) == "Fizz", "6 is divisible by 3"
-    assert checkio(5) == "Buzz", "5 is divisible by 5"
-    assert checkio(7) == "7", "7 is not divisible by 3 or 5"
+    assert fizz_buzz(15) == "Fizz Buzz", "15 is divisible by 3 and 5"
+    assert fizz_buzz(6) == "Fizz", "6 is divisible by 3"
+    assert fizz_buzz(5) == "Buzz", "5 is divisible by 5"
+    assert fizz_buzz(7) == "7", "7 is not divisible by 3 or 5"
