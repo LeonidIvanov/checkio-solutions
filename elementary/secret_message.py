8837e4f31053eecf03e2e05dde4cd93c74e848d9
LeonidIvanov <git@github.com:LeonidIvanov/python0709.git>
2018-05-03 15:11:35 +0300

    Updated "Secret Message" docstring

--- elementary/secret_message.py
+++ elementary/secret_message.py
@@ -1,9 +1,13 @@
 def find_message(text):
-    """Ever tried to send a secret message to someone without using the postal service? You could use newspapers to tell your secret. Even if someone finds your message, it's easy to brush them off and that its paranoia and a bogus conspiracy theory. One of the simplest ways to hide a secret message is to use capital letters. Let's find some of these secret messages.
+    """Ever tried to send a secret message to someone without using the postal service?
+    You could use newspapers to tell your secret.
+    Even if someone finds your message, it's easy to brush them off and that its paranoia and a bogus conspiracy theory.
+    One of the simplest ways to hide a secret message is to use capital letters. Let's find some of these secret messages.
 
     You are given a chunk of text. Gather all capital letters in one word in the order that they appear in the text.
 
-    For example: text = "How are you? Eh, ok. Low or Lower? Ohhh.", if we collect all of the capital letters, we get the message "HELLO".
+    For example: text = "How are you? Eh, ok. Low or Lower?
+    Ohhh.", if we collect all of the capital letters, we get the message "HELLO".
 
     Input: A text as a string (unicode).
 
